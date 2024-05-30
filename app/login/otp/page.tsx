08d005a938630f93aa1861bf9408f7fa2792eb1b
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-05-30 13:30:01 +0530

    Create chage password page

--- app/login/otp/page.tsx
+++ app/login/otp/page.tsx
@@ -5,7 +5,7 @@ import { BackgroundGradient } from "../../../components/ui/background-gradient";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import Link from "next/link";
 
-export default function otp() {
+export default function Otp() {
     return (
         <section className=" h-screen bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
