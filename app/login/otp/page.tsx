a44b1a1413d55140985641361e3df7df2660544d
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-05-30 12:11:48 +0530

    create otp input page ui

---
+++ app/login/otp/page.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import React from "react";
+import { BackgroundGradient } from "../../../components/ui/background-gradient";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import Link from "next/link";
+
+export default function otp() {
+    return (
+        <section className=" h-screen bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <BackgroundGradient className="rounded-[22px] max-w-lg p-4 bg-white dark:bg-zinc-900">
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                OTP Authentication
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="otp" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Check your mailbox for otp number</label>
+                                    <div className="h-20 grid grid-cols-6 gap-2 content-center">
+                                        <input type="text" name="otp" id="otp-1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                                        <input type="text" name="otp" id="otp-1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                                        <input type="text" name="otp" id="otp-1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                                        <input type="text" name="otp" id="otp-1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                                        <input type="text" name="otp" id="otp-1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                                        <input type="text" name="otp" id="otp-1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                                    </div>
+
+                                </div>
+                                <div className="h-16 grid grid-cols-2 gap-6 content-between">
+                                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Resend OTP</button>
+                                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Continue</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </BackgroundGradient>
+            </div>
+            <BackgroundBeams />
+        </section>
+    );
+}
